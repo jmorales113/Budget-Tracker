54de006a73fa4abe6396faa5e14f5d0ca87aadd5
jmorales113 <jmorales113@gmail.com>
2020-05-15 12:03:37 -0700

    Added database for deployment

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget"
+
+mongoose.connect(MONGODB_URI)
 
 const PORT = process.env.PORT || 3000
 
